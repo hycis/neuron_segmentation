aa706d3f06d3cdd3bedb6c062fb840bb9ebca2e6
Lucas Tan <lucastan@users.noreply.github.com>
2016-09-28 21:43:02 +0800

    updated

--- Model.py
+++ Model.py
@@ -29,7 +29,7 @@ class Tester(object):
         self.model = mod
 
 
-    def generate_mask(self, image_dims, input_images_path, output_images_path):
+    def generate_mask(self, image_dims, input_images_path, output_processed_path, output_masks_path):
         mod = self.model
         db = self.db
         task_id = self.task_id
@@ -55,12 +55,27 @@ class Tester(object):
             file_path = os.path.join(input_images_path, filename)
             img = cv2.imread(file_path)
             img = cv2.resize(img, (w,h))
+            out_processed_file_path = os.path.join(output_processed_path, basename + '.jpg')
+            cv2.imwrite(out_processed_file_path, img)
+
             img = np.rollaxis(img, 2, 0)
             mask = mod.fprop([img])
             mask = (mask > 0.5).astype(int)
 
-            out_file_path = os.path.join(output_images_path, basename + '.png')
-            cv2.imwrite(out_file_path, np.rollaxis(mask[0], 0, 3) * 255.0)
+            out_file_path = os.path.join(output_masks_path, basename + '.png')
+            mask_img = np.rollaxis(mask[0], 0, 3) * 255.0
+            #cv2.imwrite(out_file_path, mask_img)
+            z = np.zeros((w, h, 4))
+
+            for i in xrange(0, w):
+                for j in xrange(0, h):
+                    for k in xrange(0, 3):
+                        z[i][j][k] = mask_img[i][j][0]
+                    z[i][j][3] = 127 if mask_img[i][j][0] > 127 else 0
+
+            #out_file_path = os.path.join(output_masks_path, basename + '.alpha.png')
+            cv2.imwrite(out_file_path, z)
+
 
             processedCount = processedCount + 1
             result = db.tasks.update_one({
