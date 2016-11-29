a38985b941bfa38d6b0801855cf95c1c9af92b37
hycis <hyciswu@gmail.com>
2016-11-30 01:56:24 +0800

    added fcn for iris

---
+++ iterative_cnn/data.py
@@ -0,0 +1,73 @@
+
+from PIL import Image
+import numpy as np
+import tensorgraph as tg
+
+
+class Iris(object):
+
+
+    def __init__(self, patch_size=(32,32), min_pos_ratio=0.2, num_patch_per_img=200,
+                 train_valid = [5,1], batchsize=32, shuffle=True):
+        self.patch_size = patch_size
+        self.min_pos_ratio = min_pos_ratio
+        self.num_patch_per_img = num_patch_per_img
+        self.data_dir = '../data/manual'
+        self.train_valid = train_valid
+        self.batchsize = batchsize
+        self.shuffle = shuffle
+
+
+    def make_data(self):
+        test_nums = ['01', '02', '05', '06', '09']
+        train_num = ['21', '22', '25', '26', '29']
+        labels_path = ['{}/{}_manual1.gif'.format(self.data_dir,num) for num in train_num]
+        images_path = ['{}/{}_training.tif'.format(self.data_dir,num) for num in train_num]
+        labels = []
+        images = []
+        for img_path, lbl_path in zip(images_path, labels_path):
+            lbl = Image.open(lbl_path)
+            lbl = np.asarray(lbl)[:,:,np.newaxis]
+            lbl = lbl / 255
+            labels.append(lbl)
+
+            im = Image.open(img_path)
+            im = np.asarray(im) / 255
+            images.append(im)
+
+        img_patches = []
+        lbl_patches = []
+        for img, lbl, num in zip(images, labels, train_num):
+            print '..processing img {}'.format(num)
+            count = 0
+            h, w, c = lbl.shape
+            while count < self.num_patch_per_img:
+                y = np.random.randint(0, h-self.patch_size[0])
+                x = np.random.randint(0, w-self.patch_size[1])
+                lbl_crop = lbl[y:y+self.patch_size[0], x:x+self.patch_size[1], :]
+                if lbl_crop.mean() > self.min_pos_ratio:
+                    lbl_patches.append(lbl_crop)
+                    img_crop = img[y:y+self.patch_size[0], x:x+self.patch_size[1], :]
+                    img_patches.append(img_crop)
+                    count += 1
+
+        img_patches = np.asarray(img_patches)
+        lbl_patches = np.asarray(lbl_patches)
+
+        if self.shuffle:
+            print '..shuffling'
+            np.random.seed(1012)
+            shf_idx = np.arange(len(img_patches))
+            np.random.shuffle(shf_idx)
+            img_patches = img_patches[shf_idx]
+            lbl_patches = lbl_patches[shf_idx]
+
+        num_train = float(self.train_valid[0]) / sum(self.train_valid) * len(img_patches)
+        num_train = int(num_train)
+        train = tg.SequentialIterator(img_patches[:num_train], lbl_patches[:num_train], batchsize=self.batchsize)
+        valid = tg.SequentialIterator(img_patches[num_train:], lbl_patches[num_train:], batchsize=self.batchsize)
+        return train, valid
+
+if __name__ == '__main__':
+    data = Iris()
+    data.make_data()
---
+++ iterative_cnn/fcn.py
@@ -0,0 +1,118 @@
+
+import tensorgraph as tg
+from tensorgraph.layers import Conv2D, RELU, Sigmoid, Template
+from tensorgraph.utils import valid, same
+from tensorgraph import ProgressBar
+import tensorflow as tf
+import numpy as np
+from data import Iris
+
+class ResNet(Template):
+
+    def __init__(self, num_blocks):
+        self.num_blocks = num_blocks
+        self.blocks = []
+        for _ in range(self.num_blocks):
+            layers = []
+            layers.append(Conv2D(input_channels=3, num_filters=8, kernel_size=(5,5), stride=(1,1), padding='SAME'))
+            layers.append(RELU())
+            layers.append(Conv2D(input_channels=8, num_filters=3, kernel_size=(5,5), stride=(1,1), padding='SAME'))
+            layers.append(RELU())
+            self.blocks.append(layers)
+
+
+    def _train_fprop(self, state_below):
+        for block in self.blocks:
+            out = state_below
+            for layer in block:
+                out = layer._train_fprop(out)
+            state_below = out + state_below
+        return out
+
+
+    def _test_fprop(self, state_below):
+        for block in self.blocks:
+            out = state_below
+            for layer in block:
+                out = layer._test_fprop(out)
+            state_below = out + state_below
+        return out
+
+
+def train():
+
+    batchsize = 64
+    learning_rate = 0.001
+    max_epoch = 10
+    epoch_look_back = 3
+    percent_decrease = 0.01
+    patch_size = [32, 32]
+    train_valid = [5, 1]
+
+    X_ph = tf.placeholder('float32', [None] + patch_size + [3])
+    M_ph = tf.placeholder('float32', [None] + patch_size + [1])
+
+    h, w = 32, 32
+
+    model = tg.Sequential()
+    model.add(ResNet(num_blocks=1))
+    model.add(Sigmoid())
+
+
+    M_train_s = model.train_fprop(X_ph)
+    M_valid_s = model.test_fprop(X_ph)
+
+    train_mse = tf.reduce_mean((M_ph - M_train_s)**2)
+    valid_mse = tf.reduce_mean((M_ph - M_valid_s)**2)
+
+    data = Iris(batchsize=batchsize, patch_size=patch_size, train_valid=train_valid)
+    data_train, data_valid = data.make_data()
+
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(train_mse)
+
+    with tf.Session() as sess:
+        init = tf.initialize_all_variables()
+        sess.run(init)
+        es = tg.EarlyStopper(max_epoch=max_epoch,
+                             epoch_look_back=epoch_look_back,
+                             percent_decrease=percent_decrease)
+        for epoch in range(max_epoch):
+            print 'epoch:', epoch
+            print '..training'
+            pbar = ProgressBar(len(data_train))
+            n_exp = 0
+            train_mse_score = 0
+            for X_batch, M_batch in data_train:
+                feed_dict={X_ph:X_batch, M_ph:M_batch}
+                sess.run(optimizer, feed_dict=feed_dict)
+                train_mse_score += sess.run(train_mse, feed_dict=feed_dict) * len(X_batch)
+                n_exp += len(X_batch)
+                pbar.update(n_exp)
+            train_mse_score /= n_exp
+            print 'mean train mse:', train_mse_score
+
+
+            print '..validating'
+            pbar = ProgressBar(len(data_valid))
+            n_exp = 0
+            valid_mse_score = 0
+            for X_batch, M_batch in data_valid:
+                feed_dict={X_ph:X_batch, M_ph:M_batch}
+                valid_mse_score += sess.run(valid_mse, feed_dict=feed_dict) * len(X_batch)
+                n_exp += len(X_batch)
+                pbar.update(n_exp)
+            valid_mse_score /= n_exp
+            print 'mean valid mse:', valid_mse_score
+
+            if es.continue_learning(valid_error=valid_mse_score):
+                print 'epoch', epoch
+                print 'valid error so far:', valid_mse_score
+                print 'best epoch last update:', es.best_epoch_last_update
+                print 'best valid last update:', es.best_valid_last_update
+
+            else:
+                print 'training done!'
+                break
+
+if __name__ == '__main__':
+    train()
