61ec21a3e795ed4f148b6760d8bbc1119a59485e
Wu <zhenzhou.wu@sap.com>
2017-02-20 01:36:24 +0800

    update

--- iterative_cnn/resnet_conv3d.py
+++ iterative_cnn/resnet_conv3d.py
@@ -15,10 +15,14 @@ class ResNet(Template):
         self.blocks = []
         for _ in range(self.num_blocks):
             layers = []
-            layers.append(Conv3D(input_channels=1, num_filters=16, kernel_size=(5,5,5), stride=(1,1,1), padding='SAME'))
+            layers.append(Conv3D(input_channels=1, num_filters=8, kernel_size=(3,3,3), stride=(1,1,1), padding='SAME'))
+            layers.append(RELU())
+            layers.append(Conv3D(input_channels=8, num_filters=8, kernel_size=(3,3,3), stride=(1,1,1), padding='SAME'))
+            layers.append(RELU())
+            layers.append(Conv3D(input_channels=8, num_filters=8, kernel_size=(3,3,3), stride=(1,1,1), padding='SAME'))
             layers.append(RELU())
             # layers.append(BatchNormalization(layer_type='conv', dim=16, short_memory=0.01))
-            layers.append(Conv3D(input_channels=16, num_filters=1, kernel_size=(5,5,5), stride=(1,1,1), padding='SAME'))
+            layers.append(Conv3D(input_channels=8, num_filters=1, kernel_size=(3,3,3), stride=(1,1,1), padding='SAME'))
             layers.append(RELU())
             self.blocks.append(layers)
 
@@ -77,7 +81,7 @@ def train():
 
 
     model = tg.Sequential()
-    model.add(ResNet(num_blocks=5))
+    model.add(ResNet(num_blocks=1))
     model.add(Sigmoid())
 
 
