8c2c078190eaaad36e2d653f4b6dc0cf3eabbfe1
Wu <zhenzhou.wu@sap.com>
2017-02-20 00:29:49 +0800

    update

--- iterative_cnn/resnet_conv3d.py
+++ iterative_cnn/resnet_conv3d.py
@@ -58,7 +58,7 @@ def train():
     max_epoch = 100
     epoch_look_back = None
     percent_decrease = 0.01
-    d, h, w = 10, 10, 10
+    d, h, w = 20, 20, 20
     min_density = 0.10
     num_patch_per_img = 200
 
