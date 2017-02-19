1782398368d621be6086c7a70febc4bc124fbde1
Wu <zhenzhou.wu@sap.com>
2017-02-19 23:48:28 +0800

    update

--- iterative_cnn/data.py
+++ iterative_cnn/data.py
@@ -335,7 +335,8 @@ class DataBlks(object):
         # import pdb; pdb.set_trace()
         num_patch_per_img = np.prod(X_npy.shape) / (self.height*self.width*self.depth)
         print('number patches before:', num_patch_per_img)
-        num_patch_per_img = 1000 if num_patch_per_img > 1000 else num_patch_per_img
+        # num_patch_per_img = 1000 if num_patch_per_img > 1000 else num_patch_per_img
+        num_patch_per_img /= 100
         print('number patches picked:', num_patch_per_img)
         ttl_num_patches_tried = 0
         while count < num_patch_per_img:
