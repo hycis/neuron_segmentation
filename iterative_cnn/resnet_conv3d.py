434a17c646db9d28516b3979f2fb615f5411944b
Wu <zhenzhou.wu@sap.com>
2017-02-24 23:26:07 +0800

    20170224_232607

--- iterative_cnn/resnet_conv3d.py
+++ iterative_cnn/resnet_conv3d.py
@@ -222,15 +222,25 @@ def train(dt):
                                 print(ytrue.shape)
                                 print(ypred.shape)
 
+
                                 if i+d == z:
                                     ypred = ypred[:d-z_pad,:,:,:]
                                     ytrue = ytrue[:d-z_pad,:,:,:]
+                                    print(ytrue.shape)
+                                    print(ypred.shape, '/n')
                                 if j+h == y:
                                     ypred = ypred[:,:h-y_pad,:,:]
                                     ytrue = ytrue[:d-z_pad,:,:,:]
+                                    print(ytrue.shape)
+                                    print(ypred.shape, '/n')
                                 if k+w == x:
                                     ypred = ypred[:,:,:w-x_pad,:]
                                     ytrue = ytrue[:d-z_pad,:,:,:]
+                                    print(ytrue.shape)
+                                    print(ypred.shape, '/n')
+                                print(ytrue.shape)
+                                print(ypred.shape, '----/n')
+
                                 ypred = (ypred > threshold).astype(int)
                                 P += ytrue.sum()
                                 TP += (ypred * ytrue).sum()
